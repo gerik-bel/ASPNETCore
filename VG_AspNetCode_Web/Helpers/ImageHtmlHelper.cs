e0effb4f7f395ec13b31c0dd287a45d7cfead6e8
Vladimir.Gerasimov <Vladimir.Gerasimov@thomsonreuters.com>
2020-04-10 12:54:47 +0300

    Module 3, 03

---
+++ VG_AspNetCode_Web/Helpers/ImageHtmlHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VG_AspNetCore_Web.Helpers
+{
+    public static class ImageHtmlHelper
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper htmlHelper,
+             int imageId, string labelText)
+        {
+            return new HtmlString($"<a href='images/{imageId}'>{labelText}</a>");
+        }
+    }
+}
---
+++ VG_AspNetCode_Web/Helpers/ImageTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace VG_AspNetCore_Web.Helpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class ImageTagHelper : TagHelper
+    {
+        public int NorthwindId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", $"images/{NorthwindId}");
+            base.Process(context, output);
+        }
+    }
+}
